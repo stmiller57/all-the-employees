037704e5623c1a5759b9ebdc905b5830b8229e4b
Reformed Scribe <stmiller57@gmail.com>
2020-07-11 12:15:55 -0400

    Added Jumbotron component for employee page header

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid bg-primary">
+            <div className="container">
+                <h1 className="display-4 text-center text-white">All The Employees</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
